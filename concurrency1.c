1245b17ad39080257a141d22729c1d79334bf025
Bradley Imai <imaib@onid.oregonstate.edu>
2017-04-20 10:39:20 -0700

    start of concurrency1

---
+++ concurrency1.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Concurrency Assignment #1
+ * CS444
+ * Creators: Bradley Imai & Daniel Ross
+ * 4/20/2017
+ ******************************************************************************/
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+//check to see if the buffer is empty or not. buffer can only hold 32 items
+
+//producer will need to wait a random time between 3-7 seconds
+
+/*both the consumer and producer are not able to access the buffer
+* at the same time. One will have to lcoked and the other is unlocked
+*/
+
+
+ struct Data{
+   int number;
+   int wait_time;
+ };
+
+void producer(){
+
+}
+
+void consumer(){
+
+}
+
+int main(){
+
+}
