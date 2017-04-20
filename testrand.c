7069f321d2e9af07a60850ab3069a1a83c56b7cd
Bradley Imai <imaib@onid.oregonstate.edu>
2017-04-20 16:37:54 -0700

    updated testrand.c

--- testrand.c
+++ testrand.c
@@ -4,8 +4,43 @@
  #include <string.h>
  #include <limits.h>
  #include "mt19937ar.h"
+ #include <time.h>
 
+unsigned int randomval(){
+	unsigned int value;
+	unsigned int eax;
+	unsigned int ebx;
+	unsigned int ecx;
+	unsigned int edx;
 
+	char vendor[13];
+
+	eax = 0x01;
+
+	__asm__ __volatile__(
+	                     "cpuid;"
+	                     : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+	                     : "a"(eax)
+	                     );
+
+	if(ecx & 0x40000000){
+    printf ("asm /n");
+		__asm__("rdrand  %[value]"
+			   : [value] "=r" (value)
+			   :
+			   : "cc"
+			   );
+	}
+	else{
+    unsigned long seed = (unsigned long)time(NULL);
+		//use mt19937
+    printf ("mt19937 \n");
+		init_genrand(seed);
+		unsigned long rand = genrand_int32();
+		value = rand & UINT_MAX;
+	}
+	return value;
+}
 
 unsigned int randomminmax(unsigned int min, unsigned int max){
 	unsigned int value;
@@ -17,39 +52,7 @@ unsigned int randomminmax(unsigned int min, unsigned int max){
 	unsigned int range = (max - min)+1;
 	value = randomval();
 	value = (value%range)+min;
-	return value;	
-}
-unsigned int randomval(){
-	unsigned int value;
-	unsigned int eax;
-	unsigned int ebx;
-	unsigned int ecx;
-	unsigned int edx;
-
-	char vendor[13];
-	
-	eax = 0x01;
-
-	__asm__ __volatile__(
-	                     "cpuid;"
-	                     : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-	                     : "a"(eax)
-	                     );
-	
-	if(ecx & 0x40000000){
-		__asm__("rdrand  %[value]"
-			   : [value] "=r" (value)
-			   :
-			   : "cc"  
-			   );
-	}
-	else{
-		//use mt19937
-		init_genrand(seed);
-		unsigned long rand = genrand_int32();
-		value = rand & UINT_MAX;
-	}
-	return value;	
+	return value;
 }
 
 int main(){
@@ -57,6 +60,6 @@ int main(){
 	a = randomval();
 	b = randomminmax(2,9);
 	c = randomminmax(3,7);
-	printf("a: ",a,"   b: ",b,"   c: ",c);
+	printf("a: %u \n b: %u \n c: %u \n",a,b,c);
 	return 0;
 }
